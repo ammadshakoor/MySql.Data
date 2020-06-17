12fd77fa1f809be2674a231fabffac453039371d
Ammad Shakoor <ammadshakoor44@gmail.com>
2020-06-17 11:53:17 +0500

    Add files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,46 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!this.IsPostBack)
+        {
+            try
+            {
+                string constr = ConfigurationManager.ConnectionStrings["connectionStr"].ConnectionString;
+                using (MySqlConnection con = new MySqlConnection(constr))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand("SELECT * FROM wp_wc_order_stats"))
+                    {
+                        using (MySqlDataAdapter sda = new MySqlDataAdapter())
+                        {
+                            cmd.Connection = con;
+                            sda.SelectCommand = cmd;
+                            using (DataTable dt = new DataTable())
+                            {
+                                sda.Fill(dt);
+                                GridView1.DataSource = dt;
+                                GridView1.DataBind();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (MySql.Data.MySqlClient.MySqlException exception)
+            {
+                Label2.Text = exception.ToString();
+            }
+
+
+        }
+    }
+}
